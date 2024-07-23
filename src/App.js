41d60e16f4f2f13bbadc30af5b0fe0e21209aa43
jieunyy <107592904+jieunyy@users.noreply.github.com>
2024-07-23 14:38:13 +0900

    feat: kitchen zoom & page navigation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BeforeLogin from './pages/before_login';
 import AfterLogin from './pages/after_login';
-import MyKitchen from './pages/my_kitchen'
+import MyKitchen from './pages/my_kitchen';
+import MySink from './pages/kitchen/my_sink';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<BeforeLogin />} />
+      <Route path="/ori" element={<BeforeLogin />} />
       <Route path="/room" element={<AfterLogin />} />
-      <Route path="/kitchen" element={<MyKitchen />} />
+      <Route path="/" element={<MyKitchen />} />
+      <Route path="/kitchen/sink" element={<MySink />} />
     </Routes>
   </BrowserRouter>
 );
--- src/loadModelsKitchen.js
+++ src/loadModelsKitchen.js
@@ -4,7 +4,6 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { TextureLoader } from 'three';
 
-// 수정: 모델 로드 함수를 정의합니다.
 export function loadModelsKitchen(scene, onAllModelsLoaded) {
     const gltfLoader = new GLTFLoader();
     const objLoader = new OBJLoader();
@@ -12,30 +11,12 @@ export function loadModelsKitchen(scene, onAllModelsLoaded) {
     const textureLoader = new TextureLoader();
 
     const models = [
-        { loader: objLoader, path: '/assets/obj/kitchen/sink/sink.obj', mtlPath: '/assets/obj/kitchen/sink/sink.mtl', scale: 4.5, position: { x: -7, y: -5, z: 0 },  rotation: { x: 0, y: 1.57, z: 0 }},
-        { loader: objLoader, path: '/assets/obj/kitchen/wine/wine.obj', mtlPath: '/assets/obj/kitchen/wine/wine.mtl', scale: 0.1, position: { x: -0.5, y: -1, z: -6.5 }, rotation: { x: 1.57, y: -3.14, z: 0 } },
+        { name: 'sink', loader: objLoader, path: '/assets/obj/kitchen/sink/sink.obj', mtlPath: '/assets/obj/kitchen/sink/sink.mtl', scale: 4.5, position: { x: -7, y: -5, z: 0 },  rotation: { x: 0, y: 1.57, z: 0 }},
+        //{ loader: objLoader, path: '/assets/obj/kitchen/wine/wine.obj', mtlPath: '/assets/obj/kitchen/wine/wine.mtl', scale: 0.1, position: { x: -0.5, y: -1, z: -6.5 }, rotation: { x: 1.57, y: -3.14, z: 0 } },
         { loader: objLoader, objPath: '/assets/obj/kitchen/fridge/fridge.obj', mtlPath: '/assets/obj/kitchen/fridge/fridge.mtl', scale: 0.5, position: { x: -5.8, y: -5, z: 5.2 }, rotation: { x: Math.PI / 2 - 3.14, z: Math.PI / 2 }},
         { loader: objLoader, objPath: '/assets/obj/kitchen/diningtable/diningtable.obj', mtlPath: '/assets/obj/kitchen/diningtable/diningtable.mtl', scale: 0.1, position: { x: 4, y: -5, z: 1 }, rotation: { x: 0, y: 1.57, z: 0 }},
         { loader: objLoader, objPath: '/assets/obj/kitchen/oven/oven.obj', mtlPath: '/assets/obj/kitchen/oven/oven.mtl', scale: 5.8, position: { x: -7.2, y: -6, z: -3 }, rotation: { x: 0, y: 1.57, z: 0 } },
         { loader: objLoader, objPath: '/assets/obj/kitchen/kitchenclock/kitchenclock.obj', mtlPath: '/assets/obj/kitchen/kitchenclock/kitchenclock.mtl', scale: 0.1, position: { x: 1, y: 5, z: -7 }, rotation: { x: 0, y: Math.PI + 1.57, z: 1.57 }},
-
-        // { loader: objLoader, path: 'public/assets/obj/floorlamp/floorlamp.obj', scale: 0.1, position: { x: -4, y: -5, z: -6.5 }, rotation: { y: Math.PI / 2 } },
-        // { loader: objLoader, path: 'public/assets/obj/bookshelf/bookshelf.obj', scale: 3.8, position: { x: 5, y: -5, z: -5 }, },
-        // {
-        //     loader: objLoader,
-        //     objPath: 'public/assets/obj/imac/imac.obj',
-        //     texturePath: 'public/assets/obj/imac/texture/', // 텍스처 파일이 있는 디렉토리 경로
-        //     textures: [
-        //         'base.jpg',
-        //         'screen.jpg',
-        //         'keyboardnum.jpg',
-        //         'keyboardon.jpg',
-        //         'mouse.jpg'
-        //     ],
-        //     scale: 0.05,
-        //     position: { x: -3.5, y: -2, z: 0 },
-        //     rotation: { y: Math.PI - 3 }
-        // },
     ];
 
     let loadedModels = 0;
@@ -58,6 +39,7 @@ export function loadModelsKitchen(scene, onAllModelsLoaded) {
                         });
                         object.scale.set(model.scale, model.scale, model.scale);
                         object.position.set(model.position.x, model.position.y, model.position.z);
+                        object.name = model.name || '';
 
                         if (model.rotation) {
                             object.rotation.set(
@@ -89,6 +71,7 @@ export function loadModelsKitchen(scene, onAllModelsLoaded) {
                     object.scene = object.scene || object;
                     object.scene.scale.set(model.scale, model.scale, model.scale);
                     object.scene.position.set(model.position.x, model.position.y, model.position.z);
+                    object.scene.name = model.name || '';
 
                     if (model.rotation) {
                         object.scene.rotation.set(
