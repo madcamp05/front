a559610ef7bd933312189e776ebdf30719e1689b
jieunyy <107592904+jieunyy@users.noreply.github.com>
2024-07-24 17:28:31 +0900

    feat: bookshelf added

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ import AfterLogin from './pages/after_login';
 import MyKitchen from './pages/my_kitchen';
 import MySink from './pages/kitchen/my_sink';
 import MyGameRoom from './pages/mygameroom';
-import IMacCanvas from './pages/myroom_effects/imac';
-import MyBed from './pages/myroom_effects/bed';
-import Bookshelf from './pages/myroom_effects/bookshelf';
-import MoonLight from './pages/myroom_effects/ moonlight';
-import BananaLight from './pages/myroom_effects/ moonlight';
+import IMacCanvas from './pages/myroom/imac';
+import MyBed from './pages/myroom/bed';
+import Bookshelf from './pages/myroom/bookshelf';
+import MoonLight from './pages/myroom/ moonlight';
+import BananaLight from './pages/myroom/ moonlight';
 import MyTable from './pages/kitchen/my_table';
 import MyFridge from './pages/kitchen/my_fridge';
 import MyOven from './pages/kitchen/my_oven';
+import BookshelfScene from './pages/myroom/bookshelf';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/ori" element={<BeforeLogin />} />
-      {/* <Route path="/" element={<BeforeLogin />} /> */}
-      {/* <Route path="/" element={<AfterLogin />} /> */}
-      <Route path="room" element={<MyGameRoom />} />
-      {/* <Route path="/" element={<MyKitchen />} /> */}
+      <Route path="/room" element={<AfterLogin />} /> 
+      <Route path="/" element={<BookshelfScene />} /> 
 
+      <Route path="/game" element={<MyGameRoom />} />
       <Route path="/game/light" element={<BananaLight />} />
-      <Route path="/room" element={<AfterLogin />} />
+
       <Route path="/kitchen" element={<MyKitchen />} />
       <Route path="/kitchen/sink" element={<MySink />} />
       <Route path="/kitchen/table" element={<MyTable />} />
