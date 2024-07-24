d4c4ae24ea0e8f88df9b2dbf0a29182533686dbc
jieunyy <107592904+jieunyy@users.noreply.github.com>
2024-07-25 01:50:31 +0900

    feat: sidebar ver1

---
+++ src/sidebar.js
@@ -0,0 +1,13 @@
+// Sidebar.js
+import React from 'react';
+
+const Sidebar = () => (
+  <div className="sidebar">
+    {/* Your 3D buttons */}
+    <button>Button 1</button>
+    <button>Button 2</button>
+    <button>Button 3</button>
+  </div>
+);
+
+export default Sidebar;
